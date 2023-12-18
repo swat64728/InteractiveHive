cd961f0a109fa6e5d93eb05771e0a986e6ad39c3
swat64728 <103403590+swat64728@users.noreply.github.com>
2023-12-18 18:32:13 +0300

    Add files via upload

---
+++ javascript/settings.js
@@ -0,0 +1,7 @@
+var primaryColor = "rgb(25, 25, 25)";
+var secondaryColor = "rgb(0, 255, 0)";
+var thirtiaryColor = "rgb(50, 50, 50)";
+
+document.documentElement.style.setProperty('--primary-color', primaryColor);
+document.documentElement.style.setProperty('--secondary-color', secondaryColor);
+document.documentElement.style.setProperty('--thirtiary-color', thirtiaryColor);
