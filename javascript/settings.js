c9ea74b7bb5644e1c89884e2558d929e198287a6
swat64728 <103403590+swat64728@users.noreply.github.com>
2023-12-18 19:36:57 +0300

    Update settings.js

--- javascript/settings.js
+++ javascript/settings.js
@@ -2,6 +2,13 @@ var primaryColor = "rgb(25, 25, 25)";
 var secondaryColor = "rgb(0, 255, 0)";
 var thirtiaryColor = "rgb(50, 50, 50)";
 
+var screenHeight = window.screen.height;
+
+const ratioFactor = 0.2;
+var barHeight = screenHeight * ratioFactor;
+
 document.documentElement.style.setProperty('--primary-color', primaryColor);
 document.documentElement.style.setProperty('--secondary-color', secondaryColor);
 document.documentElement.style.setProperty('--thirtiary-color', thirtiaryColor);
+
+document.documentElement.style.setProperty('--barHeight',barHeight)
